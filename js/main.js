d436be81d5626d6683f32e6cc6f9bf14b54ca039
zhangzongjian <1102349546@qq.com>
2018-05-13 16:54:21 +0800

    初步完成页面部分

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 /** 数据初始化 **/
 //用户
 var myScore = 2;
+var server = "localhost/videoplay/";
 //图库
-var imgMainArr = [
-	"http://placehold.it/1920x1080",
-	"http://placehold.it/920x880",
-	"http://placehold.it/1920x1080",
-	"http://placehold.it/786x1080"
-];
-$("#img-main").attr("src", imgMainArr[0]);
+var imgMainArr = [];
+//$("#img-main").attr("src", imgMainArr[0]);
 //播放器
 var videoPlaying = {id:"1000002", src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"};
-$("#video").attr("src", videoPlaying.src);
-$("#videoPlayingId").html(videoPlaying.id);
-//播放区
+//$("#video").attr("src", videoPlaying.src);
+//$("#videoPlayingId").html(videoPlaying.id);
+//播放区 每页8个
+var pager = {pageNo:2, pageSum:10, pageEach:8};
 var videoArr = [
 	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
 	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:0, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
@@ -21,9 +18,11 @@ var videoArr = [
 	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
 	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:0, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
 	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:1, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:0, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:1, src:"http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4"},
 ];
-var videoHtml = "";
 var imgHtml;
+var videoHtml = "";
 videoArr.forEach(function(video){
 	imgHtml = "";
 	video.imgs.forEach(function(imgSrc, index){
@@ -37,12 +36,24 @@ videoArr.forEach(function(video){
     '</div>';
 });
 $("#videoList").html(videoHtml);
-//下载区
+$("#videoPager").html(getPagerHtml(pager));
+
+
+//下载区 每页8个
+var pager = {pageNo:1, pageSum:11, pageEach:8};
 var downloadArr = [
-	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/share/init?surl=F4haUzfN8lv30hS4AGZE_g", size:"335M"},
-	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/share/init?surl=F4haUzfN8lv30hS4AGZE_g", size:"335M"},
-	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/share/init?surl=F4haUzfN8lv30hS4AGZE_g", size:"335M"},
-	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/share/init?surl=F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
+	{id:"1000002", imgs:["http://placehold.it/250x140", "http://placehold.it/1920x1080", "http://placehold.it/786x1080"], score:5, downloadUrl:"https://pan.baidu.com/s/1F4haUzfN8lv30hS4AGZE_g", size:"335M"},
 ]
 var downloadHtml = "";
 var imgHtml;
@@ -55,25 +66,32 @@ downloadArr.forEach(function(download){
 	'<div class="cell gallery">' +
 	 imgHtml +
      '<h5 class="font-size-small"> ID:' + download.id + ' <span class="size">大小：' + download.size + '</span> </h5>' +
-     '<p class="font-size-small"> <a href="' + download.downloadUrl+ '">百度网盘</a> <span class="downloadCode">提取码：????</span> </p> ' +
+     '<p class="font-size-small"> <a href="' + download.downloadUrl+ '" target="_blank">百度网盘</a> <span class="downloadCode">提取码：????</span> </p> ' +
      '<a href="javascript:void(0)" class="button small expanded hollow">提取码<span class="downloadScore">（<font>' + download.score + '</font>积分）</span></a> ' +
     '</div>';
 });
 $("#downloadList").html(downloadHtml);
+$("#downloadPager").html(getPagerHtml(pager));
 /** 数据初始化 end **/	
 
 /** 图片操作 **/
 var imgIndex = 0;
 var imgLen = imgMainArr.length;
 $(".prevImg").click(function(){
-	imgIndex--;
-	if(imgIndex < 0) imgIndex = imgLen - 1;
-	$("#img-main").attr("src", imgMainArr[imgIndex]);
+	if(imgLen >0) {
+		imgIndex--;
+		if(imgIndex < 0) imgIndex = imgLen - 1;
+		$("#img-main").attr("src", imgMainArr[imgIndex]);
+	}
+	else noData();
 });
 $(".nextImg").click(function(){
-	imgIndex++;
-	if(imgIndex > imgLen - 1) imgIndex = 0;
-	$("#img-main").attr("src", imgMainArr[imgIndex]);
+	if(imgLen >0) {
+		imgIndex++;
+		if(imgIndex > imgLen - 1) imgIndex = 0;
+		$("#img-main").attr("src", imgMainArr[imgIndex]);
+	}
+	else noData();
 });
 /** 图片操作 end **/
 
@@ -96,7 +114,7 @@ video.onpause = function() {
 	$("#videocenter-text").html(pause);
 	$("#videocenter").show(); 
 };
-video.onwaiting = function() { 
+video.onwaiting = function() {
 	if(timerId == -1) {
 		timerId = window.setInterval(function(){
 			$("#videocenter-text").html(waiting[i++]);
@@ -121,14 +139,47 @@ $("#videocenter").click(function(){
 	}
 	else if("随便看看" == $("#videocenter-text").html())
 	{
-		video.src = "http://vd3.bdstatic.com/mda-iba247rx1ffpppb7/mda-iba247rx1ffpppb7.mp4";
+		video.src = "#";
 		video.play();
 	}
 });
 //清除
 $("#clearVideo").click(function() {
+	if(timerId != -1) {
+		window.clearInterval(timerId);
+		timerId = -1;
+		i = 0;
+	}
 	video.src = "";
 	$("#videocenter-text").html(randomView);
 	$("#videocenter").show(); 
 });
 /** 视频操作 end **/
+
+/** 翻页操作 **/
+function getPagerHtml(pager) {
+	var optionHtml;
+	var pagerHtml =
+		(pager.pageNo!=1?'<li> <a href="javascript:goPage(1)">首页</a> </li> ':'') +
+		(pager.pageNo!=1?'<li> <a href="javascript:goPage(' + (pager.pageNo-1) + ')">上一页</a> </li> ':'') +
+			'<select class="form-control" onchange="goPage(this.value)">' +
+				function(){
+					optionHtml = "";
+					for(var i = 1; i<=pager.pageSum; i++) {
+						optionHtml += '<option value="' + i + '" ' + (i==pager.pageNo?'selected':'') + '>' + i + '</option>';
+					}
+					return optionHtml;
+				}() +
+			'</select> ' +
+		(pager.pageNo!=pager.pageSum?'<li> <a href="javascript:goPage(' + (pager.pageNo+1) + ')">下一页</a> </li> ':'') +
+		(pager.pageNo!=pager.pageSum?'<li> <a href="javascript:goPage(' + pager.pageSum + ')">末页</a> </li> ':'');
+	return pagerHtml;
+}
+function goPage(pageNo) {
+	alert(pageNo);
+}
+/** 翻页操作 end **/
+
+function noData() {
+	alert("数据不存在");
+}
